717d8cb3bc4a9aed12f9c6e1f8e9b338e9944df0
user146479 <user146479@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-11-29 09:41:15 -0500

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by user146479 on 11/29/18.
+//  Copyright © 2018 user146479. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
